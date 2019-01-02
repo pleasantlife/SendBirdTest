2e45235103b6f44c12dce8592a50a1b947ccd26f
pleasantlife <pleasantlife@gmail.com>
2019-01-02 22:18:57 +0900

    190102 SendBird test update.(Openchannel and Groupchannel in one recyclerview)

--- app/src/main/java/com/gandan/android/sendbirdtest/ActivityMain.java
+++ app/src/main/java/com/gandan/android/sendbirdtest/ActivityMain.java
@@ -2,36 +2,135 @@ package com.gandan.android.sendbirdtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.gandan.android.sendbirdtest.Adapter.ChannelRecyclerAdapter;
+import com.sendbird.android.BaseChannel;
+import com.sendbird.android.GroupChannel;
+import com.sendbird.android.GroupChannelListQuery;
+import com.sendbird.android.OpenChannel;
+import com.sendbird.android.OpenChannelListQuery;
 import com.sendbird.android.SendBird;
 import com.sendbird.android.SendBirdException;
 import com.sendbird.android.User;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityMain extends AppCompatActivity {
+
+    EditText nameEditText;
+    Button editNameBtn;
+    List<GroupChannel> groupChannelList;
+    List<OpenChannel> openChannelList;
+    ChannelRecyclerAdapter channelRecyclerAdapter;
+    RecyclerView channelRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        channelRecyclerView = findViewById(R.id.channelRecyclerView);
+
         SendBird.init(getString(R.string.sendbird_app_id), this);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-DD hh:mm:dd");
-        String userId = sdf.format(System.currentTimeMillis());
+        nameEditText = findViewById(R.id.nameEditText);
+        if("".equals(nameEditText.getText().toString())) {
+            nameEditText.setEnabled(true);
+        } else {
+            nameEditText.setEnabled(false);
+        }
+        editNameBtn = findViewById(R.id.editNameBtn);
 
-        SendBird.connect("2019-01-01 11:27:01", new SendBird.ConnectHandler() {
+        editNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nameEditText.isEnabled()){
+                    nameEditText.setEnabled(false);
+                    connectSendBird(nameEditText.getText().toString());
+                } else {
+                    nameEditText.setEnabled(true);
+                }
+            }
+        });
+
+
+    }
+
+    private void makeGroupChannel(User user){
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        GroupChannel.createChannel(userList, false, "Hello Group", null, null, null, new GroupChannel.GroupChannelCreateHandler() {
+            @Override
+            public void onResult(GroupChannel groupChannel, SendBirdException e) {
+                if( e == null){
+                    getGroupChannelList();
+                } else {
+                    Log.e("makeError", e.getMessage()+"");
+                }
+            }
+        });
+    }
+
+    private void connectSendBird(String userId){
+        SendBird.connect(userId, new SendBird.ConnectHandler() {
             @Override
             public void onConnected(User user, SendBirdException e) {
                 if( e == null) {
                     Toast.makeText(ActivityMain.this, user.getUserId() + "", Toast.LENGTH_SHORT).show();
+                    makeGroupChannel(user);
                 } else {
                     Log.e("SendBirdError", e.getMessage()+"");
                 }
             }
         });
     }
+
+    private void getGroupChannelList(){
+        GroupChannelListQuery groupChannelListQuery = GroupChannel.createMyGroupChannelListQuery();
+        groupChannelListQuery.setIncludeEmpty(true);
+        groupChannelListQuery.next(new GroupChannelListQuery.GroupChannelListQueryResultHandler() {
+            @Override
+            public void onResult(List<GroupChannel> list, SendBirdException e) {
+                if (e == null){
+                    groupChannelList = list;
+                    getOpenChannelList();
+                } else {
+                    Log.e("GroupError", e.getMessage()+"");
+                }
+            }
+        });
+    }
+
+    private void getOpenChannelList(){
+        OpenChannelListQuery openChannelListQuery = OpenChannel.createOpenChannelListQuery();
+        openChannelListQuery.next(new OpenChannelListQuery.OpenChannelListQueryResultHandler() {
+            @Override
+            public void onResult(List<OpenChannel> list, SendBirdException e) {
+                if( e == null){
+                    openChannelList = list;
+                    setRecyclerView();
+                } else {
+                    Log.e("OpenError", e.getMessage()+"");
+                }
+            }
+        });
+    }
+
+    private void setRecyclerView(){
+        channelRecyclerAdapter = new ChannelRecyclerAdapter(this, groupChannelList, openChannelList);
+        channelRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        channelRecyclerView.setAdapter(channelRecyclerAdapter);
+        channelRecyclerAdapter.notifyDataSetChanged();
+    }
+
+
 }
---
+++ app/src/main/java/com/gandan/android/sendbirdtest/Adapter/ChannelRecyclerAdapter.java
@@ -0,0 +1,78 @@
+package com.gandan.android.sendbirdtest.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sendbird.android.GroupChannel;
+import com.sendbird.android.OpenChannel;
+
+import java.util.List;
+
+public class ChannelRecyclerAdapter extends RecyclerView.Adapter<ChannelRecyclerAdapter.ChannelRecyclerHolder> {
+
+    Context context;
+    List<GroupChannel> groupChannelList;
+    List<OpenChannel> openChannelList;
+    int size = 0;
+
+    public ChannelRecyclerAdapter(Context context, List<GroupChannel> groupChannelList, List<OpenChannel> openChannelList){
+        this.context = context;
+        this.groupChannelList = groupChannelList;
+        this.openChannelList = openChannelList;
+        this.size = groupChannelList.size() + openChannelList.size();
+    }
+
+
+    @NonNull
+    @Override
+    public ChannelRecyclerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ChannelRecyclerHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChannelRecyclerHolder holder, int position) {
+        if(position >= groupChannelList.size()){
+            final OpenChannel openChannel = openChannelList.get(position - groupChannelList.size());
+            holder.text1.setText(openChannel.getName());
+            holder.text1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, openChannel.getCreatedAt()+"", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            final GroupChannel groupChannel = groupChannelList.get(position);
+            holder.text1.setText(groupChannel.getName());
+            holder.text1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, groupChannel.getCreatedAt()+"", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return size;
+    }
+
+    class ChannelRecyclerHolder extends RecyclerView.ViewHolder {
+
+        TextView text1;
+
+        public ChannelRecyclerHolder(View itemView) {
+            super(itemView);
+
+            text1 = itemView.findViewById(android.R.id.text1);
+        }
+    }
+}
